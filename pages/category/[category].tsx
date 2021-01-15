59f1f017b403fead8e01eab1c22b693c009b35b8
Nia RAMAROSANDY <ramarosandynia@gmail.com>
2021-01-15 16:24:14 +0100

    Generate category page at build time

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -1,12 +1,24 @@
 import Head from 'next/head';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { ReactElement } from 'react';
 import { useRouter } from 'next/router';
 
-import CategoryArticles from '../../components/category/CategoryArticles';
-import SubCategories from '../../components/category/SubCategories';
-import { reverseFileName } from '../../helpers/fileHelpers';
+import ArticleList from '../../components/article/ArticleList';
+import CategoryList from '../../components/category/CategoryList';
+import allSubCategories from '../../data/subCategories.json';
+import categories from '../../data/categories.json';
+import { ArticleItem } from '../../components/article/ArticleListItem';
+import { formatFileName, reverseFileName } from '../../helpers/fileHelpers';
 
-export default function Category(): ReactElement {
+type Props = {
+  subCategories: string[];
+  articles: ArticleItem[];
+};
+
+export default function Category({
+  subCategories,
+  articles,
+}: Props): ReactElement {
   const router = useRouter();
   const { category } = router.query;
   const formatedCategory = reverseFileName(category as string);
@@ -18,8 +30,42 @@ export default function Category(): ReactElement {
       </Head>
 
       <h1>Category : {formatedCategory}</h1>
-      <SubCategories category={formatedCategory} />
-      <CategoryArticles category={formatedCategory} />
+
+      <h3>Subcategories</h3>
+      <CategoryList categories={subCategories} />
+
+      <h3>Articles</h3>
+      <ArticleList articles={articles} />
     </>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: categories.map((category) => ({
+    params: { category: formatFileName(category) },
+  })),
+  fallback: false,
+});
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  async function loadArticles(category: string) {
+    const categoryFileName = formatFileName(category);
+
+    try {
+      const { default: data } = await import(
+        `../../data/articlesByCategory/${categoryFileName}.json`
+      );
+
+      return data;
+    } catch (e) {
+      return [];
+    }
+  }
+
+  return {
+    props: {
+      subCategories: allSubCategories[params.category as string],
+      articles: await loadArticles(params.category as string),
+    },
+  };
+};
