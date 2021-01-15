be89190d508658e7180230470b0e464bf42d4fac
Nia RAMAROSANDY <ramarosandynia@gmail.com>
2021-01-15 16:39:53 +0100

    Generate wiki page at build time

--- pages/wiki/[...sections].tsx
+++ pages/wiki/[...sections].tsx
@@ -1,20 +1,28 @@
 import Head from 'next/head';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { ReactElement } from 'react';
 import { useRouter } from 'next/router';
 
-import ArticleContent from '../../components/article/ArticleContent';
+import articles from '../../data/articles.json';
 import { formatFileName, reverseFileName } from '../../helpers/fileHelpers';
 
-export default function Article(): ReactElement {
+type Props = {
+  article: ArticleContent;
+};
+
+type ArticleContent = {
+  html: string;
+  categories: string[];
+};
+
+export default function Article({ article }: Props): ReactElement {
   const router = useRouter();
   const { sections } = router.query;
   const nonEmptySections = (sections as string[])?.filter((a) => a) ?? [];
 
-  if (!nonEmptySections.length) {
+  if (!article?.html) {
     return null;
   }
-
-  const formatedArticle = formatFileName(nonEmptySections.join('/'));
 
   return (
     <>
@@ -23,7 +31,47 @@ export default function Article(): ReactElement {
           {nonEmptySections.map((a) => reverseFileName(a)).join(' - ')}
         </title>
       </Head>
-      <ArticleContent articleTitle={formatedArticle} />;
+
+      {/* eslint-disable-next-line react/no-danger */}
+      <div dangerouslySetInnerHTML={{ __html: article.html }} />
     </>
   );
 }
+
+type PathParams = {
+  sections: string[];
+};
+
+export const getStaticPaths: GetStaticPaths<PathParams> = async () => ({
+  paths: articles.map((article) => ({
+    params: { sections: article.split('/').map((a) => formatFileName(a)) },
+  })),
+  fallback: false,
+});
+
+export const getStaticProps: GetStaticProps<Props, PathParams> = async ({
+  params,
+}) => {
+  async function loadArticleContent(title: string): Promise<ArticleContent> {
+    try {
+      const { default: data } = await import(
+        `../../data/articlesContent/${title}.json`
+      );
+
+      return data;
+    } catch (e) {
+      return {
+        html: '',
+        categories: [],
+      };
+    }
+  }
+
+  const title = formatFileName(params.sections.join('/'));
+
+  return {
+    props: {
+      article: await loadArticleContent(title),
+    },
+  };
+};
